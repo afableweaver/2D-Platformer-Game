c459e14374978a40476fe2fc416ad00aa6d2fd62
afableweaver <shruti.bhat365@gmail.com>
2022-01-30 11:20:55 +0530

    C4-MA2 - Tile Map Setup
    Using Tilemap terrain, adding colliders, bg elements

---
+++ Assets/Script/GroundCheck.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public bool isGrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+        Debug.Log("Is grounded");
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+        Debug.Log("FLYING"+collision.gameObject.name);
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,46 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     //public variables for access
-    public float speed;
-    public bool crouch;
-    public float jump;
+    public float speed;         //player speed
+    public bool crouch;         //crouch bool
+    public float jump;          //player jump force value
+    public float horizontal;    //player horizontal movement
+    private float vertical;     //player vertical movement
+    private Vector2 position;   //player position
+
     private Rigidbody2D rb2d;
+    private BoxCollider2D boxcollider;
+    public GroundCheck GroundStatus;
+
 
     private void Awake()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        boxcollider = gameObject.GetComponent<BoxCollider2D>();
     }
     private void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Jump");
-        bool crouch = Input.GetKey(KeyCode.LeftControl);
-        MoveChar(horizontal, vertical);
+        //assigning variables
+        horizontal = Input.GetAxisRaw("Horizontal");
+        crouch = Input.GetKey(KeyCode.LeftControl);
+        vertical = Input.GetAxisRaw("Jump");
+
         PMAnimation(horizontal, vertical, crouch);
     }
 
-    private void MoveChar(float horizontal, float vertical)
-    {
+    private void FixedUpdate()
+    {   
+        //CHARACTER MOVEMENT
+
         // player horizontal movement
         Vector2 position = transform.position;
         position.x += horizontal * speed * Time.deltaTime;
         transform.position = position;
 
         //player vertical movement
-        if (vertical>0)
+        if (vertical > 0 && GroundStatus.isGrounded == true)
         {
-            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Force) ;
+            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
         }
         else
         {
 
         }
-    }    
+    }
 
     private void PMAnimation(float horizontal, float vertical, bool crouch)
     {
@@ -57,8 +66,22 @@ public class PlayerController : MonoBehaviour
         }
         transform.localScale = scale;
 
-        //player crouch
+        //player crouch and collider resize
         animator.SetBool("Crouching", crouch);
+        Vector2 collscale = boxcollider.size;
+        Vector2 colloffset = boxcollider.offset;
+        if (crouch==true)
+        {
+            collscale = new Vector2(0.9480562f, 1.25814f);
+            colloffset = new Vector2(-0.120859f, 0.5891516f);
+        }
+        else
+        {
+            collscale = new Vector2(0.63787f,2.02f);
+            colloffset = new Vector2(0.02717397f, 0.9700818f);
+        }
+        boxcollider.size = collscale;
+        boxcollider.offset = colloffset;
 
         //player jump
         if (vertical > 0)
@@ -71,4 +94,5 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+  
 }
