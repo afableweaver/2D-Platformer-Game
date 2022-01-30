1518a6826b0dc85ea83288ecc8429391fb97fd9e
afableweaver <shruti.bhat365@gmail.com>
2022-01-30 11:55:30 +0530

    C4-MA1 - To Next Level
    Created Scenemanager that allows player to go from one level to another on level completion.

--- Assets/Script/GroundCheck.cs
+++ Assets/Script/GroundCheck.cs
@@ -7,12 +7,12 @@ public class GroundCheck : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         isGrounded = true;
-        Debug.Log("Is grounded");
+        //Debug.Log("Is grounded");
     }
     
     private void OnTriggerExit2D(Collider2D collision)
     {
         isGrounded = false;
-        Debug.Log("FLYING"+collision.gameObject.name);
+        //Debug.Log("FLYING"+collision.gameObject.name);
     }
 }
---
+++ Assets/Script/LevelOverController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelOverController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            //Debug.Log("GameComplete");
+            SceneManager.LoadScene("Scene2");
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
